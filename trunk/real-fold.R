39f33211521a9fcf415be1126a31df39b2b1f0c5
sterratt <sterratt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-04 12:31:27 +0000

    * Got rid of cruft and reordered
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/real-fold.R
+++ trunk/real-fold.R
@@ -1,8 +1,5 @@
 source("../../data/Anatomy/cluster-analysis.R")
 
-map <- as.matrix(read.map("../../data/Anatomy/ALU/M643-4/CONTRA"))
-plot.map(map)
-
 map.to.segments <- function(map) {
   segs <- list()
   i <- 1
@@ -31,7 +28,7 @@ norm2 <- function(v) {
 ## Find angle between two vectors
 find.angle <- function(v1, v2) {
   sp <- (t(v1) %*% v2) / (norm2(v1) * norm2(v2))
-  print(sp)
+  ##  print(sp)
   return(acos(sp))
 }
 
@@ -39,9 +36,9 @@ find.corners <- function(map) {
   angle <- rep(NA, nrow(map))
   for (i in 2:(nrow(map)-1)) {
     v1 <- map[i,] - map[i-1,]
-    print(v1)
+    ## print(v1)
     v2 <- map[i+1,] - map[i,]
-    print(v2)
+    ## print(v2)
     angle[i] <-  find.angle(v1, v2) 
   }
   return(angle)
@@ -64,43 +61,7 @@ check.intersection <- function(x1, y1, x2, y2) {
   return(FALSE)
 }
 
-segs <- map.to.segments(map)
-## map <- join.segments(list(segs[[1]], segs[[3]], segs[[4]]))
-##map <- segs[[1]]
-palette(rainbow(180))
-angles <- find.corners(map) * 180/pi
-
-##text(map[,1], map[,2], labels=format(angles,digits=2))
-## points(map[,1], map[,2], col=angles, pch=20)
-
-
-## Make a grid of indicies
-## Specify maxium elevation and grid spacing in degrees
-theta.max.deg <- 140 
-dtheta.deg <- 10 
-dphi.deg   <- 10 
-r <- 10000                                # radius
-
-## Make some indicies
-is <- 1:(360/dphi.deg)
-js <- 1:(theta.max.deg/dtheta.deg)
-
-dphi <-   dphi.deg * pi/180
-dtheta <- dtheta.deg * pi/180
-theta.max <- theta.max.deg * pi/180
-
-## The matricies X and Y specify the mapping from point i,j in polar space
-## to cartesian coordinates
-X <- matrix(NA, length(is), length(js))
-Y <- matrix(NA, length(is), length(js))
-
-gmap <- expand.grid(i=is, j=js)
-gmap <- cbind(gmap,
-              phi=  gmap[,"i"]*dphi,
-              theta=gmap[,"j"]*dtheta)
-gmap <- cbind(gmap, X=gmap[,"theta"]*cos(gmap[,"phi"]))
-gmap <- cbind(gmap, Y=gmap[,"theta"]*sin(gmap[,"phi"]))
-
+## Plot a mesh of radial and tangential lines 
 plot.mesh <- function(gmap) {
   ## Plot the tangential lines
   for (j in js) {
@@ -114,9 +75,44 @@ plot.mesh <- function(gmap) {
   }
 }
 
-## Plot the grid
-## plot(NA,NA,xlim=c(-pi, pi), ylim=c(-pi, pi))
-plot.mesh(gmap)
+## Read in data
+map <- as.matrix(read.map("../../data/Anatomy/ALU/M643-4/CONTRA"))
+
+## Corner analysis
+segs <- map.to.segments(map)
+edge <- join.segments(list(segs[[1]], segs[[3]], segs[[4]]))
+##map <- segs[[1]]
+palette(rainbow(180))
+angles <- find.corners(edge) * 180/pi
+
+## Make a grid of indicies
+## Specify maxium elevation and grid spacing in degrees
+theta.max.deg <- 140 
+dtheta.deg <- 10 
+dphi.deg   <- 10 
+r <- 1000                                # radius
+
+## Make some indicies
+is <- 1:(360/dphi.deg)
+js <- 1:(theta.max.deg/dtheta.deg)
+
+## Convert to radians
+phi <-   dphi.deg * pi/180
+theta <- dtheta.deg * pi/180
+theta.max <- theta.max.deg * pi/180
+
+## The matricies X and Y specify the mapping from point i,j in polar space
+## to cartesian coordinates
+X <- matrix(NA, length(is), length(js))
+Y <- matrix(NA, length(is), length(js))
+
+## Put these into gmap structure
+gmap <- expand.grid(i=is, j=js)
+gmap <- cbind(gmap,
+              phi=  gmap[,"i"]*phi,
+              theta=gmap[,"j"]*theta)
+gmap <- cbind(gmap, X=r*gmap[,"theta"]*cos(gmap[,"phi"]))
+gmap <- cbind(gmap, Y=r*gmap[,"theta"]*sin(gmap[,"phi"]))
 
 ## Find the neigbours and put them in columns
 ## - nl - tangential left (in direction of increasing phi)
@@ -132,8 +128,6 @@ gmap[gmap[,"nu"]>nrow(gmap),"nu"] <- NA
 gmap[,"nd"] <- (1:nrow(gmap))- M
 gmap[gmap[,"nd"]<1,"nd"] <- NA
 
-
-
 ## Computes the distance between grid points if they were
 ## spread over the surface of a sphere
 ## Find distance to left neigbours
@@ -143,6 +137,8 @@ gmap[,"Lu"] <- r * dtheta
 gmap[is.na(gmap[,"nu"]),"Lu"] <- NA
 gmap[,"Ld"] <- r * dtheta
 gmap[is.na(gmap[,"nd"]),"Ld"] <- NA
+
+## Fitting of mesh to data
 
 ## Convert to proximity matricies A and B
 ## Points 1:((N-1)*M) are variable points
@@ -164,113 +160,17 @@ P <-  mgmap[((N-1)*M+1):(N*M),c("X","Y")]
 ## solution points
 Q <- solve(D - A) %*% B %*% P
 
+## Put new distances back in map for plotting
 mgmap[,c("X","Y")] <- rbind(Q, P)
 
-plot(NA,NA,xlim=c(-pi, pi), ylim=c(-pi, pi))
+## Plotting
+plot.map(map)
+
+##text(edge[,1], edge[,2], labels=format(angles,digits=2))
+points(edge[,1], edge[,2], col=angles, pch=20)
+
 plot.mesh(mgmap)
-x11()
-## Compute force on each element
-compute.force <- function(gmap) {
-  ## Compute the force due to a component
-  compute.force.component <- function(X1, Y1, n, l, L) {
-  X2 <- gmap[gmap[,n],"X"]
-  Y2 <- gmap[gmap[,n],"Y"]
-  gmap[,l] <<- sqrt((X2 - X1)^2 + (Y2-Y1)^2)
-  fac <- (gmap[,l] - gmap[,L]) / (gmap[,l] * gmap[,L])
-  F.X <- fac * (X2 - X1) 
-  F.Y <- fac * (Y2 - Y1)
-  F.X[is.na(F.X)] <- 0
-  F.Y[is.na(F.Y)] <- 0
-  return(cbind(F.X, F.Y))
-}
-  X1 <- gmap[,"X"]
-  Y1 <- gmap[,"Y"]
 
-  Fl <- compute.force.component(X1, Y1, "nl", "ll", "Ll")
-  Fr <- compute.force.component(X1, Y1, "nr", "lr", "Lr")
-  Fu <- compute.force.component(X1, Y1, "nu", "lu", "Lu")
-  Fd <- compute.force.component(X1, Y1, "nd", "ld", "Ld")
-
-##  print(Fl)
-  Ftot <- Fl + Fr + Fu + Fd
-  
-  gmap[,"F.X"] <- Ftot[,1]
-  gmap[,"F.Y"] <- Ftot[,2]
-  return(gmap)
-}
-
-## Compute energy
-compute.energy <- function(X1, Y1, gmap) {
-  ## Compute the energy due to a component
-  compute.energy.component <- function(X1, Y1, n, l, L) {
-    X2 <- X1[gmap[,n]]
-    Y2 <- Y1[gmap[,n]]
-    l <- sqrt((X2 - X1)^2 + (Y2 - Y1)^2)
-    E <- (l - gmap[,L])^2 / (2 * gmap[,L])
-    E[is.na(E)] <- 0
-    return(E)
-  }
-
-  El <- compute.energy.component(X1, Y1, "nl", "ll", "Ll")
-  Er <- compute.energy.component(X1, Y1, "nr", "lr", "Lr")
-  Eu <- compute.energy.component(X1, Y1, "nu", "lu", "Lu")
-  Ed <- compute.energy.component(X1, Y1, "nd", "ld", "Ld")
-
-  Etot <- sum(El + Er + Eu + Ed)
-  print(Etot)
-  return(Etot)
-}
-
-compute.energy.gr <- function(X1, Y1, gmap) {
-  compute.energy.gr.component <- function(X1, Y1, n, L) {
-    X2 <- X1[gmap[,n]]
-    Y2 <- Y1[gmap[,n]]
-    l <- sqrt((X2 - X1)^2 + (Y2 - Y1)^2)
-    L <- gmap[,L]
-    dE <-   (l - L)/ (l * L) * c(X1 - X2, Y1 - Y2)
-    dE[is.na(dE)] <- 0
-    return(dE)
-  }
-  dEl <- compute.energy.gr.component(X1, Y1, "nl", "Ll")
-  dEr <- compute.energy.gr.component(X1, Y1, "nr", "Lr")
-  dEu <- compute.energy.gr.component(X1, Y1, "nu", "Lu")
-  dEd <- compute.energy.gr.component(X1, Y1, "nd", "Ld")
-
-  dE <- dEl + dEr + dEu + dEd
-  return(dE)
-}
-
-compute.total.energy <- function(p, gmap) {
-  X <- p[1:nrow(gmap)]
-  Y <- p[(1:nrow(gmap))+nrow(gmap)]
-  return(compute.energy(X, Y, gmap))
-}
-
-compute.total.energy.gr <- function(p, gmap) {
-  X <- p[1:nrow(gmap)]
-  Y <- p[(1:nrow(gmap))+nrow(gmap)]
-  return(compute.energy.gr(X, Y, gmap))
-}
-
-
-
-dt <- 0.001
-opt <- list(par = c(gmap[,"X"],gmap[,"Y"]) * r)
-if (FALSE) {
-for (time in 1:1000) {
-  print(time)
-##  plot.map(map)
-  plot(NA,NA,xlim=r*c(-pi, pi), ylim=r*c(-pi, pi))
-  plot.mesh(gmap)
-  opt <- optim(opt$par,
-               method="CG",
-               fn=compute.total.energy,
-               gr=compute.total.energy.gr,
-               control=list(maxit=10), gmap=gmap)
-  gmap[,"X"] <- opt$par[1:nrow(gmap)]
-  gmap[,"Y"] <- opt$par[(1:nrow(gmap))+nrow(gmap)]
-  ##  gmap <- compute.force(gmap)
-##  gmap[,"X"] <- gmap[,"X"] + dt * gmap[,"F.X"]
-##  gmap[,"Y"] <- gmap[,"Y"] + dt * gmap[,"F.Y"]
-}
-}
+## Plot the grid
+## plot(NA,NA,xlim=c(-pi, pi), ylim=c(-pi, pi))
+## plot.mesh(gmap)
