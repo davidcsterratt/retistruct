562e7ee9f47a0ee2e2523196d5460c290362b6b2
sterratt <sterratt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-08 16:30:28 +0000

    * Put hemisphere-creation code in create.hemisphere
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/hemisphere.R
+++ trunk/hemisphere.R
@@ -1,85 +1,76 @@
 require(rgl)
 require(geometry)
 
-fd <- function(p, ...) sqrt((p^2)%*%c(1,1,1)) - 1
-                                        # also predefined as `mesh.dsphere'
-fd <- function(p, phi, ...) {
-  ## also predefined as `mesh.dsphere'
-  d <- sqrt((p^2)%*%c(1,1,1)) - 1
-  d <- d + (sqrt((p^2)%*%c(1,1,0)) < cos(phi)) * (p[,3] > 0) *
-    (p[,3] - sin(phi) - (sqrt((p^2)%*%c(1,1,1)) - 1))
-  return(d)
-}
-
-## fd <- function(p, ...) {
-##   out <- sqrt((p^2)%*%c(1,1,1)) - 1 + (sqrt((p^2)%*%c(1,1,0)) < 0.5) * (p[,3] > 0)
-
-  ## print((sqrt((p^2)%*%c(1,1,0)) < 0.5) * (p[,3] > 0))
-  ## out <- sqrt((p^2)%*%c(1,1,1)) - 1
-  ## print(dim(p))
-  ## print(dim(out))
-##  return(out)
-##}
-                                        # also predefined as `mesh.dsphere'
-fh <- function(p, phi, ...) {
-  h <- rep(1,nrow(p))
-##   h <- h + 0.5 * (sqrt((p^2)%*%c(1,1,0)) < sin(phi)) * (p[,3] > 0)
-  return(h)
-}
-                                        # also predefined as `mesh.hunif'
-
-tri.area <- function(P, Pt) {
-  A <- P[Pt[,1],]
-  B <- P[Pt[,2],]
-  C <- P[Pt[,3],]
-  AB <- cbind(B-A, 0)
-  AC <- cbind(C-A, 0)
-  return(0.5 * sqrt(apply(extprod3d(AB, AC))^2, 1, sum))
-}
-
 ## Find the normals of triangles
 tri.normals <- function(P, Pt) {
   A <- P[Pt[,1],]
   B <- P[Pt[,2],]
   C <- P[Pt[,3],]
-
   AB <- cbind(B-A)
   AC <- cbind(C-A)
-  print(dim(AB))
-  print(dim(AC))  
   return(extprod3d(AB, AC))
 }
 
-## Create the hemisphere
-phi <- 50 * pi/180
-nfix <- round(cos(phi)* 2 * pi / 0.24 - 1)
-pfix <- cbind(cos(phi) * sin(2*pi*(1:nfix)/nfix),
-              cos(phi) * cos(2*pi*(1:nfix)/nfix),
-              sin(phi))
-bbox <- matrix(c(-1,1),2,3)
-P <- distmeshnd(fd,fh,0.2,bbox, maxiter=100, pfix=pfix, phi=phi)
-Pt <- delaunayn(P)
+## Find the areas of triangles
+tri.areas <- function(P, Pt) {
+  return(0.5 * sqrt(apply(tri.normals(P, Pt))^2, 1, sum))
+}
 
-## This is the triagulation of the surface
-Ptri = surf.tri(P, Pt)
-normals <- tri.normals(P, Ptri)
+## Create a hemisphere cut off at longitude phi
+create.hemisphere <- function(phi=40*pi/180) {
+  ## Distance function for sphere which is flattened at a lattitude phi
+  ## also predefined as `mesh.dsphere'
+  fd <- function(p, phi, ...) {
+    d <- sqrt((p^2)%*%c(1,1,1)) - 1
+    d <- d + (sqrt((p^2)%*%c(1,1,0)) < cos(phi)) * (p[,3] > 0) *
+      (p[,3] - sin(phi) - (sqrt((p^2)%*%c(1,1,1)) - 1))
+    return(d)
+  }
 
-## areas <- tri.area(P, Pt[,1:3])
-ptri <- Ptri[atan2(normals[,3], sqrt(apply(normals[,1:2]^2, 1, sum))) < (pi/2 - 0.001),]
-## ptri <- Ptri
+  ## Uniform length function
+  ## also predefined as `mesh.hunif'
+  fh <- function(p, phi, ...) {
+    h <- rep(1,nrow(p))
+    return(h)
+  }
 
-##i.rm <- which(p[,3]>0.5)
-##pt <- pt[!apply(matrix(pt %in% i.rm, nrow(pt), 4), 1, any),]
-##p[i.rm,] <- c(NA, NA, NA)
+  ## A number of fixed points are chosen around the rim, 
+  ## which is the line where the flattened part joins
+  ## the sphere proper. The number of fixed points is chosen so
+  ## that the intervals along the rim are approximately the same
+  ## as the internode spacing
+  nfix <- round(cos(phi)* 2 * pi / 0.24 - 1)
+  pfix <- cbind(cos(phi) * sin(2*pi*(1:nfix)/nfix),
+                cos(phi) * cos(2*pi*(1:nfix)/nfix),
+                sin(phi))
+  bbox <- matrix(c(-1,1),2,3)
 
-## Create the asymmetric connectivity matrix
-Cu <- rbind(ptri[,1:2], ptri[,2:3], ptri[,c(1,3)])
-Cu <- Unique(Cu)
+  ## This fuction from the geometry package creates the mesh, seeking
+  ## to make the links as equal in length as possible
+  P <- distmeshnd(fd,fh,0.2,bbox, maxiter=100, pfix=pfix, phi=phi)
+  Pt <- delaunayn(P)
 
-## Find lengths of connections
-Ls <- sqrt(apply((P[Cu[,1],] - P[Cu[,2],])^2, 1, sum))
+  ## This is the triagulation of the surface
+  Ptri = surf.tri(P, Pt)
+  normals <- tri.normals(P, Ptri)
 
-ptri <- t(ptri)
+  ## Remove upward-facing triangles as they are on the flattened part
+  ptri <- Ptri[atan2(normals[,3], sqrt(apply(normals[,1:2]^2, 1, sum))) < (pi/2 - 0.001),]
+
+  ## Create the asymmetric connectivity matrix
+  Cu <- rbind(ptri[,1:2], ptri[,2:3], ptri[,c(1,3)])
+  Cu <- Unique(Cu)
+
+  ## Find lengths of connections
+  Ls <- sqrt(apply((P[Cu[,1],] - P[Cu[,2],])^2, 1, sum))
+
+  return(list(P=P, Ptri=ptri, Cu=Cu, Ls=Ls))
+}
+
+hs <- create.hemisphere()
+
+ptri <- t(hs$Ptri)
+P <- hs$P
 rgl.clear()
 rgl.triangles(P[ptri,1], P[ptri,2], P[ptri,3], col="white", front="line")
 
