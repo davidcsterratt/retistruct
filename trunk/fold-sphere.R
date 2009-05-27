80d76a1432110034620f9a4acd9ce7e56c2063ff
sterratt <sterratt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-27 09:44:17 +0000

    * Use the standardised curated data file
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/fold-sphere.R
+++ trunk/fold-sphere.R
@@ -156,15 +156,8 @@ tri.area <- function(P, Pt) {
 
 L <- 200                            # spacing between grid points
 
-## Read in data
-sys <- read.sys("../../data/Anatomy/ALU/M643-4/CONTRA")
-map <- as.matrix(read.map("../../data/Anatomy/ALU/M643-4/CONTRA"))
-
-## Corner analysis
-segs <- map.to.segments(map)
-## Some curation is required here
-segs4 <- segs[[4]][23:1,]
-edge.path <- create.path(list(segs[[1]], segs[[3]], segs4), close=TRUE)
+## Read in and curate data to give edge.path and map
+source("M634-4.R")
 
 ## All the points on the edge path apart from the first, which is repeated by the last
 P <- cbind(X=edge.path[-1,"X"], Y=edge.path[-1,"Y"])
