929cfebc6000e4f4df55a104e67e85e08be11c95
sterratt <sterratt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-12 15:58:28 +0000

    * Now sets up forward and backward tear sets
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/fold-sphere2.R
+++ trunk/fold-sphere2.R
@@ -18,39 +18,42 @@ path <- function(i, j, p) {
 ## P is the set of points describing the edge.
 ## T is the tear matrix
 stitch.retina <- function(P, T) {
-  N <- nrow(P)                          # Number of points
-
-  ## Create forward and backward pointers
-  f <- c(2:N, 1)
-  b <- c(N, 1:(N-1))
-
   ## Extract information from tear matrix
   A <- T[,1]                            # apicies of tears
   VB <- T[,2]                           # forward verticies
   VF <- T[,3]                           # backward verticies
 
+  ## Create forward and backward pointers
+  N <- nrow(P)                          # Number of points
+  f <- Mod((1:N) + 1, N) # c(2:N, 1)
+  b <- Mod((1:N) - 1, N) #c(N, 1:(N-1))
+  
+  ## Adjust forward and backward pointers appropriately
+  f[VB] <- VF
+  b[VF] <- VB
+  
   ## Create initial set of correspondances - store as matrix
   C <- cbind(VF, VB)
 
   ## Initialise the set of points in the rim
   Rset <- 1:N
   
+  ## Do not include the verticies in the tears
+  TFset <- list()
+  TBset <- list()
   ## Create sets of points for each tear and remove these points from
   ## the rim set
   for (j in 1:nrow(T)) {
-    print(paste(f[VB[j]], b[VF[j]]))
-    Tset[[j]] <- Mod(path(f[VB[j]], b[VF[j]], f), N)
-    Rset <- setdiff(Rset, Tset[[j]])
+    TFset[[j]] <- Mod(path(A[j], VF[j], f), N)
+    TBset[[j]] <- Mod(path(A[j], VB[j], b), N)
+    Rset <- setdiff(Rset, setdiff(TFset[[j]], VF[j]))
+    Rset <- setdiff(Rset, setdiff(TBset[[j]], VB[j]))
   }
 
-  ## Adjust forward and backward pointers appropriately
-  f[VB] <- VF
-  b[VF] <- VB
-
-  ## Do not include the verticies in the tears
-  Tset <- list()
-  
-  return(list(Rset=Rset, VF=VF, VB=VB, A=A))
+  return(list(Rset=Rset,
+              VF=VF, VB=VB, A=A,
+              TFset=TFset, TBset=TBset,
+              C=C))
 }
 
 ## Read in and curate data to give edge.path and map
@@ -58,13 +61,19 @@ source("M634-4.R")
 
 P <- edge.path[-nrow(edge.path),1:2]
 
-
-
 s <- stitch.retina(P, tearmat)
 
 plot(P, pch=".", cex=4)
 points(P[s$Rset,], col="red", pch=".", cex=4)
 points(P[s$VF,], col="purple", pch="+")
-points(P[s$VB,], col="purple", pch="+")
-points(P[s$A, ], col="blue", pch="+")
-
+points(P[s$VB,], col="blue", pch="+")
+points(P[s$A, ], col="cyan", pch="+")
+for (TFset in s$TFset) {
+  lines(P[TFset,], col="purple")
+}
+for (TBset in s$TBset) {
+  lines(P[TBset,], col="blue")
+}
+for (j in 1:nrow(s$C)) {
+  lines(P[s$C[j,],], col="orange")
+}
