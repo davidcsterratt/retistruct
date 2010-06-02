dd80e6f77c0920d1a226825fa113b34ebca9cafe
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-02 16:33:31 +0000

    * Removed a dataset
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@163 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/src/test-datafile-utils.R
+++ trunk/src/test-datafile-utils.R
@@ -2,16 +2,16 @@ source("datafile-utils.R")
 
 datasets <- c("../../../data/Anatomy/marked-up-retinae-2010-03-24/gm119-5-adult-C57BL6",
               "../../../data/Anatomy/marked-up-retinae-2010-03-24/gm143-2-P2-C57BL6/",
-              "../../../data/Anatomy/marked-up-retinae-2010-03-24/gm257-1-P8-C57BL6/",
-              "../../../data/Anatomy/ALU/M642-3/CONTRA")
+              "../../../data/Anatomy/marked-up-retinae-2010-03-24/gm257-1-P8-C57BL6/")
 
-##../ 	./ 	CONFIG/ 	M641-5/ 	M641-6/ 	M642-1/ 	M642-2/
+## Extra files to try later
+##M641-5/ 	M641-6/ 	M642-1/ 	M642-2/
 ##M642-3/ 	M642-4/ 	M642-5/ 	M642-6/ 	M642-7/ 	M643-1/ 	M643-2/
-##M643-3/ 	M643-4/ 	M643-5/ 	M643-6/ 	m643.pdf
+##M643-3/ 	M643-4/ 	M643-5/ 	M643-6/ 	
 
 for (dataset in datasets) {
   map <- read.map(dataset)
-  ## sys <- read.sys(dataset)
+  sys <- read.sys(dataset)
   plot.map(map, TRUE)
 
   segs <- map.to.segments(map)
