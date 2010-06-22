0e9a27617b5db28963032d3ea318c3f72695e6b2
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-22 13:32:11 +0000

    * Allow segment list to be specified as argument
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@197 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/version2/src/triangle/triangle.R
+++ branches/version2/src/triangle/triangle.R
@@ -1,13 +1,20 @@
 if (is.loaded("R_triangulate")) dyn.unload("R_triangle.so")
 dyn.load("R_triangle.so")
 
-triangulate <- function(P, a=NULL, q=NULL, Y=FALSE) {
+triangulate <- function(P, S=NULL, a=NULL, q=NULL, Y=FALSE) {
   if (ncol(P) == 2) {
     P <- t(P)
   }
   PB <- rep(1, ncol(P))
-  S <- rbind(1:ncol(P), c(2:ncol(P),1))
+  if (is.null(S)) {
+    S <- rbind(1:ncol(P), c(2:ncol(P),1))
+  } else {
+    if (ncol(S) == 2) {
+      S <- t(S)
+    }
+  }
   SB <- rep(1, ncol(S))
+
   out <- .Call("R_triangulate",
                P,
                as.integer(PB),
