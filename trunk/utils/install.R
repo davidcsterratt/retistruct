0a8fcc9cc0675666108a3f4c9c0ea1988ec0dab4
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-10 16:43:09 +0000

    * Updated installation procedure
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@294 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/utils/install.R
+++ trunk/utils/install.R
@@ -1,4 +1,12 @@
-source("config.R")
+## Set up libPaths to ensure that the libraries are installed in
+## the R_LIBS_USER directory. This can be specified using the 
+.libPaths(c(Sys.getenv("R_LIBS_USER"), .libPaths()))
+
+## Need to make sure we have a valid repository
+options(repos="http://cran.r-project.org")
+
+## It ought to be possible to get these dependendcies from the
+## retistruct DESCRIPTION, but I don't know how!!
 if (!require("foreign"))       install.packages("foreign")
 if (!require("geometry"))      install.packages("geometry")
 if (!require("gWidgets"))      install.packages("gWidgets")
@@ -6,3 +14,6 @@ if (!require("gWidgetsRGtk2")) install.packages("gWidgetsRGtk2")
 if (!require("cairoDevice"))   install.packages("cairoDevice")
 if (!require("rgl"))           install.packages("rgl")
 if (!require("plotrix"))       install.packages("plotrix")
+
+## Now install retistruct
+install.packages("retistruct_0.1.tar.gz")
