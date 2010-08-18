62a0e6326895a2cff66ca0e15db6443efc7108d4
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-18 11:08:59 +0000

    * Basic logging added
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@324 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/retistruct-batch.R
+++ trunk/retistruct/R/retistruct-batch.R
@@ -7,13 +7,23 @@ list.dirs <- function(path='.') {
   }
   return(dirs)
 }
-  
-retistruct.batch <- function(path='.') {
-  datasets <- list.dirs(path)
+
+## retistruct.bactch() - Recurse through a directory tree, determining
+## whether the directory contains valid raw data and markup, and
+## performing the reconstruction if it does
+##
+## tldir     - the top level of the tree through which to recurse
+## outputdir - directory in which to dump a log file and images
+##
+retistruct.batch <- function(tldir='.', outputdir='.') {
+  datasets <- list.dirs(tldir)
+  logdat <- data.frame()
   for (dataset in datasets) {
     ret <- system(paste("R --vanilla <<EOF
 library(retistruct)
 retistruct.cli(\"", dataset, "\", 600)
 EOF", sep=""), intern=FALSE, wait=TRUE)
+    logdat <- rbind(logdat, data.frame(Dataset=dataset, Return=ret))
+    write.csv(logdat, paste(outputdir, "/retistruct.log", sep=""))
   }
 }
