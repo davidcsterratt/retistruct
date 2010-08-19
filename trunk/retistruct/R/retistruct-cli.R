5bcafc2ba7896ed1f8f7b1e5d213634df993aa4b
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-19 12:27:39 +0000

    * retistruct.cli.figures -> retistruct.cli.figure * Incorporate retistruct.read.recdata() in retistruct.cli.figure * Use Ds and Ss from r
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@335 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/retistruct-cli.R
+++ trunk/retistruct/R/retistruct-cli.R
@@ -20,36 +20,42 @@ retistruct.cli.process <- function(outputdir=NA) {
   retistruct.reconstruct()
   retistruct.save()
   if (!is.na(outputdir)) {
-    retistruct.cli.figures(outputdir)
+    retistruct.cli.figure(outputdir)
   }
 }
 
-retistruct.cli.figures <- function(outputdir) {
-  basepath <- gsub("\\./", "", dataset)
-  basepath <- gsub("/", "_", basepath)
-  ## Flat plot
-  pdf(file=file.path(outputdir, paste(basepath, "-flat.pdf", sep="")),
-      width=6, height=6)
-  plot.outline.flat(P, gb, axt="s")
-  if (!is.null(r$t) && !is.null(r$r)) {
-    with(r, plot.gridlines.flat(t$P, t$T, r$phi, r$lambda, m$Tt, p$phi0*180/pi))
-  }
-  plot.datapoints.flat(Ds)
-  plot.landmarks.flat(Ss, col="orange")
-  title(dataset)
-  dev.off()
+## retistruct.cli.figure - Print a figure to file
+##
+## It requires the global variable dataset to be set
+retistruct.cli.figure <- function(outputdir) {
+  retistruct.read.recdata()
+  if (!is.null(r)) {
+    ## Determine the name of a figure
+    basepath <- gsub("\\./", "", dataset)
+    basepath <- gsub("/", "_", basepath)
+    
+    ## Flat plot
+    pdf(file=file.path(outputdir, paste(basepath, "-flat.pdf", sep="")),
+        width=6, height=6)
+    plot.outline.flat(r$P, r$gb, axt="s")
+    with(r, plot.gridlines.flat(P, T, phi, lambda, Tt, phi0*180/pi))
+    plot.datapoints.flat(r$Ds)
+    plot.landmarks.flat(r$Ss, col="orange")
+    title(dataset)
+    dev.off()
 
-  ## Polar plot
-  pdf(file=file.path(outputdir, paste(basepath, "-polar.pdf", sep="")),
-      width=6, height=6)
-  plot.polar(phi0)
-  if (!is.null(r$Dss)) {
-    plot.outline.polar(r)
-    plot.datapoints.polar(r$Dss, cex=5)
+    ## Polar plot
+    pdf(file=file.path(outputdir, paste(basepath, "-polar.pdf", sep="")),
+        width=6, height=6)
+    plot.polar(phi0)
+    if (!is.null(r$Dss)) {
+      plot.outline.polar(r)
+      plot.datapoints.polar(r$Dss, cex=5)
+    }
+    if (!is.null(r$Sss)) {
+      plot.landmarks.polar(r$Sss, col="orange")
+    }
+    title(dataset)
+    dev.off()
   }
-  if (!is.null(r$Sss)) {
-    plot.landmarks.polar(r$Sss, col="orange")
-  }
-  title(dataset)
-  dev.off()
 }
