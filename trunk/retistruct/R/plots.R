5ad1ec724f3f95c158da6a9f11c57ec5adc84996
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-27 16:28:22 +0000

    * Tweak to plot.l.vs.L()
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@386 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/plots.R
+++ trunk/retistruct/R/plots.R
@@ -184,13 +184,14 @@ plot.l.vs.L <- function(r) {
   palette(rainbow(100)) ## Green is about 35; dark blue about 70
   cols <- strain.colours(log(o$strain))
   with(o, plot(L, l, col=cols, pch='.', cex=5,
+               xlim=c(0, max(L, l)), ylim=c(0, max(L, l)),
                xlab="Length on flattened object",
                ylab="Length on reconstructed object", asp=1))
   par(xpd=FALSE)
   abline(0, 1)
   abline(0, 0.75, col="blue")
   abline(0, 1.25, col="red")
-  with(o, text(0.25*max(L), 0.25*max(L)*0.75, "25% compressed", col="blue",
+  with(o, text(0.7*max(L), 0.7*max(L)*0.75, "25% compressed", col="blue",
                pos=4))
   with(o, text(0.75*max(L), 0.75*max(L)*1.25, "25% expanded", col="red",
                pos=2))
