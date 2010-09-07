4158167a2e2a81d1b5405a6f31f863cd0ddb6ca8
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-07 16:29:03 +0000

    * Added the ability to have more than one triangle
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@396 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/fem/fem-rotate.R
+++ trunk/fem/fem-rotate.R
@@ -1,3 +1,8 @@
+## fem-rotate.R - FEM method with rotation 
+
+library(geometry)                       # For trimesh
+library(rgl)                       # For 3D plot
+
 ## Construct B matrix for a triangle
 construct.B <- function(x, y) {
   ## Area of triangle
@@ -38,6 +43,26 @@ construct.D <- function(E=1, nu=0) {
   return(D)
 }
 
+construct.K <- function(P, T, D) {
+  N <- ncol(P)
+  M <- nrow(T)
+  K <- matrix(0, 2*N, 2*N)
+  ## Elasticity matrix for plane stress in an isotropic material
+  for (i in 1:M) {
+    ## Start of with one triangle
+    x <- P[1, T[i,]]
+    y <- P[2, T[i,]]
+
+    B <- construct.B(x, y)
+    
+    inds <- c(2*T[i,1]-1:0,
+              2*T[i,2]-1:0,
+              2*T[i,3]-1:0)
+    K[inds,inds] <- K[inds,inds] + t(B) %*% D %*% B
+  }
+  return(K)
+}
+
 rotate <- function(theta) {
   return(rbind(c(cos(theta), -sin(theta)),
                c(sin(theta),  cos(theta))))
@@ -48,57 +73,105 @@ shear <- function(k) {
                c(0, 1)))
 }
 
+## Construct an elasticity matrix
 D <- construct.D()
 
-
-## Construct original triangle
+## Construct original set of points
 p1 <- c(0,   0)
 p2 <- c(1,   0)
 p3 <- c(0.5, 1)
-P <- cbind(p2-p1, p3-p1)
+p4 <- c(1,   1)
+P <- cbind(p1, p2, p3, p4)
 
-P2 <- cbind(p1, p2, p3)
-B <- construct.B(P2[1,], P2[2,])
-K <- t(B) %*% D %*% B
+## Triangle matrix
+T <- rbind(c(1, 2, 3),
+           c(2, 4, 3))
 
-## Construct new triangle
+## Construct B and K matricies
+K <- construct.K(P, T, D)
+
+## Construct new set of points
 q1 <- c(0,   0, 0)
 q2 <- c(1,   0, 3)
 q3 <- c(0.5, 0.2, 2)
-Q <- cbind(q2-q1, q3-q1)
+q4 <- c(2,   2, 2)
+Q <- cbind(q1, q2, q3, q4)
 
-## Find transformation M linking two
-## This means find M in
-## t(P) %*% t(M) = t(Q)
-## Which is same as
-## M %*% P = Q
-M <- t(solve(t(P), t(Q)))
+compute.force <- function(T, P, Q, K) {
+  g <- matrix(0, nrow(Q), ncol(Q))
+  for (i in 1:nrow(T)) {
+    P0 <- P[,T[i,2:3]] - P[,T[i,1]]
+    Q0 <- Q[,T[i,2:3]] - Q[,T[i,1]]
 
-## Find the rotation matrix R and unitary matrix M such that
-## M = R %*% U
-s <- svd(M)
-U <- s$v %*% diag(s$d) %*% t(s$v)
-R <- s$u %*% t(s$v)
+    ## Find transformation M linking two
+    ## This means find M in
+    ## t(P) %*% t(M) = t(Q)
+    ## Which is same as
+    ## M %*% P = Q
+    M <- t(solve(t(P0), t(Q0)))
 
-## Find P', the transformed points in the frame of reference of the
-## original triangle
-## R %*% Pp = Q
-## Hence we need
-Pp <- qr.solve(R, Q)
+    ## Find the rotation matrix R and unitary matrix M such that
+    ## M = R %*% U
+    s <- svd(M)
+    U <- s$v %*% diag(s$d) %*% t(s$v)
+    R <- s$u %*% t(s$v)
+    
+    ## Find P', the transformed points in the frame of reference of the
+    ## original triangle
+    ## R %*% Pp = Q
+    ## Hence we need
+    P0p <- qr.solve(R, Q0)
+    
+    A <- P0p - P0
+    a <- c(0, 0, as.vector(A))
 
-A <- Pp - P
-a <- c(0, 0, as.vector(A))
+    ##     The SVD decomposition of the matrix as computed by LAPACK/LINPACK,
+    ##
+    ##                                 X = U D V',                            
+    ## According to wikipedia
+    ##
+    ## R = V D V'
 
-##     The SVD decomposition of the matrix as computed by LAPACK/LINPACK,
-##
-##                                 X = U D V',                            
-## According to wikipedia
-##
-## R = V D V'
+    ## Force in frame of oringinal triangle
+    inds <- c(2*T[i,1]-1:0,
+              2*T[i,2]-1:0,
+              2*T[i,3]-1:0)
+    
+    f <- -K[inds, inds] %*% a
 
-## Force in frame of oringinal triangle
+    ## Force in frame of new triangle
+    g1 <- R %*% f[1:2]
+    g2 <- R %*% f[3:4]
+    g3 <- R %*% f[5:6]
+    g[,T[i,]] <- g[,T[i,]] + cbind(g1, g2, g3)
+  }
+  return(g)
+}
 
-f <- K %*% a
+plot.3d <- function(Q, T) {
+    rgl.clear()
+    triangles3d(matrix(Q[1,t(T)], nrow=3),
+                matrix(Q[2,t(T)], nrow=3),
+                matrix(Q[3,t(T)], nrow=3),
+                color="darkgrey")
+}
+Q.init <- Q
+graphical <- TRUE
+for (i in 1:50) {
 
-## Force in frame of new triangle
-R %*% f[1:2]
+  g <- compute.force(T, P, Q, K)
+
+  Q <- Q + 0.01*g
+
+  ## print(Q)
+  
+  if (graphical) {
+    plot.3d(Q, T)
+    ## plot.default(x=NA, y=NA,
+    ##              xlim=c(min(c(P0[1,],P0p[1,],0)), max(c(P0[1,],P0p[1,],0))),
+    ##              ylim=c(min(c(P0[2,],P0p[2,],0)), max(c(P0[2,],P0p[2,],0))))
+    
+    ## trimesh(T, rbind(0, t(P0p)), add=TRUE, col="red")
+  }
+}
+trimesh(T, rbind(0, t(P)))
