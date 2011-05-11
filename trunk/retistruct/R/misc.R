08da39dc1a663ab0382ab94dfda8eb36d43145e0
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-11 09:55:19 +0000

    * Removed merge.lists()
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@528 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/misc.R
+++ trunk/retistruct/R/misc.R
@@ -20,18 +20,6 @@ flipud <- function(M) {
   return(M[nrow(M):1,])
 }
 
-## merge.lists(l, m)
-##
-## Merge the contents of list l and list m, overwriting any names in
-## l which also occur in m
-##
-merge.lists <- function(l, m) {
-  for (n in names(m)) {
-    l[n] = m[n]
-  }
-  return(l)
-}
-
 ## Return standard names for lists containing any un-named elements
 stdnames <- function(l) {
   nl <- names(l)
