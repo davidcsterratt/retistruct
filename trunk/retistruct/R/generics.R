c8e7820d5c373bce66f6bfb51ad5a7f65157a4fc
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-17 13:55:14 +0000

    * Added plot.flat.list() for reconstructions that do not have the   correct class
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@541 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/generics.R
+++ trunk/retistruct/R/generics.R
@@ -4,3 +4,9 @@ plot.flat <- function(x, axt="n", ylim=NULL, ...) {
 
 plot.flat.default <- function(x, axt="n", ylim=NULL, ...) {
 }
+
+plot.flat.list <- function(x, axt="n", ylim=NULL, ...) {
+  plot.flat.annotatedDataset(x, axt, ylim, ...)
+}
+
+
