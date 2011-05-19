c067302d33dbc757158f4f7a0abe684598b3b8f8
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-19 12:37:33 +0000

    * OO version of plot.stitch.flat()
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@559 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/retistruct/R/StitchedOutline.R
@@ -0,0 +1,35 @@
+plot.flat.stitchedOutline <- function(s, axt="n", ylim=NULL, ...) {
+  NextMethod()
+  args <- list(...)
+  plot.stitch <- is.null(args$stitch) || args$stitch
+
+  if (plot.stitch) {
+    with(s, {
+      points(P[VF,], col="red", pch="+")
+      points(P[VB,], col="orange", pch="+")
+      points(P[V0,], col="cyan", pch="+")
+      for (TF in TFset) {
+        lines(P[TF,], col="red", ...)
+      }
+      for (TB in TBset) {
+        lines(P[TB,], col="orange", ...)
+      }
+      for (j in 1:length(h)) {
+        if (h[j] != j) {
+          lines(P[c(j, h[j]),], col="blue", ...)
+        }
+      }
+      
+      for (j in 1:length(hf)) {
+        if (hf[j] != j) {
+          lines(P[c(j, hf[j]),], col="green", ...)
+        }
+      }
+      for (j in 1:length(hb)) {
+        if (hb[j] != j) {
+          lines(P[c(j, hb[j]),], col="green", ...)
+        }
+      }
+    })
+  }
+}
