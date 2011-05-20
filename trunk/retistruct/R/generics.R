f5bd92096fb07c98237e9a9b3d0ccf0082a06700
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-20 14:08:37 +0000

    * Make plot.polar.default() give empty plot
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@584 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/generics.R
+++ trunk/retistruct/R/generics.R
@@ -19,6 +19,7 @@ plot.polar <- function(r, show.grid=TRUE,
 plot.polar.default <- function(r, show.grid=TRUE,
                                grid.col="gray", grid.bg="transparent", 
                                grid.int.minor=15, grid.int.major=45, ...) {
+  plot.new()
 }
 
 
