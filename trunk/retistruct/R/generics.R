976f57b573261bd5a8784457c0b11c0d469b67f9
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-03 14:06:19 +0000

    * Removed cruft
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@623 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/generics.R
+++ trunk/retistruct/R/generics.R
@@ -5,11 +5,6 @@ plot.flat <- function(x, axt="n", ylim=NULL, ...) {
 plot.flat.default <- function(x, axt="n", ylim=NULL, ...) {
 }
 
-## plot.flat.list <- function(x, axt="n", ylim=NULL, ...) {
-##   plot.flat.annotatedDataset(x, axt, ylim, ...)
-## }
-
-
 plot.polar <- function(r, show.grid=TRUE,
                        grid.col="gray", grid.bg="transparent", 
                        grid.int.minor=15, grid.int.major=45, ...) {
