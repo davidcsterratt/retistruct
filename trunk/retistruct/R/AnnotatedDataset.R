acc25def1a8b7d5af02d00082b97bbaf0c56207b
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-03 12:48:03 +0000

    * Remove redundant fields from AnnotatedDataset * Document functions
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@620 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/AnnotatedDataset.R
+++ trunk/retistruct/R/AnnotatedDataset.R
@@ -1,13 +1,34 @@
-AnnotatedDataset <- function(a){
+##' Construct an AnnotatedDataset that contains information specific
+##' to the dataset in question. 
+##'
+##' @title AnnotatedDataset constructor
+##' @param d A \code{dataset} object
+##' @return An \code{annotatedDataset} object. This contains all the
+##' information from \code{d} plus:
+##' \item{\code{DVflip}}{\code{TRUE} if the raw data is flipped in
+##' the dorsoventral direction} 
+##' \item{\code{side}}{The side of the eye ("Left" or "Right")}
+##' @author David Sterratt
+AnnotatedDataset <- function(d) {
+  a <- d
   class(a) <- c("annotatedDataset", class(a))
-  a$iN <- NA
-  a$iD <- NA
-  a$iOD <- NA
   a$DVflip <- FALSE
   a$side <- "Right"
   return(a)
 }
 
+##' Plot an annotated dataset. This basically is equivalent to
+##' plotting a \code{dataset}, but may perform some transformations to
+##' the date or plotting parameters. At present, if \code{DVflip} is
+##' \code{TRUE}, it flips the \emph{y}-axis.
+##'
+##' @title Flat plot of annotated dataset
+##' @param a \code{annotatedDataset} object
+##' @param axt whether to plot axes
+##' @param ylim y-limits
+##' @param ... Other plotting parameters
+##' @method plot.flat annotatedDataset
+##' @author David Sterratt
 plot.flat.annotatedDataset <- function(a, axt="n", ylim=NULL, ...) {
   if (a$DVflip) {
     if (is.null(ylim)) {
