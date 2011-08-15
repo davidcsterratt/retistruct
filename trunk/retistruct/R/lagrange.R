ccb413b03ed88a17f62162f325bc87abd1d3aebf
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-15 15:51:33 +0000

    * Added restraint function * Added ability to omit returning of all values
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@684 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/lagrange.R
+++ trunk/retistruct/R/lagrange.R
@@ -2,31 +2,50 @@
 ##'
 ##' @title Solve Lagrange's (?) equation of motion
 ##' @param x generalised coordinates of particles
-##' @param f generalised force function
+##' @param force generalised force function
+##' @param restraint function to restore positions to constraint surface
 ##' @param mu mass of all particles
 ##' @param gamma friction coefficient
 ##' @param dt time step
 ##' @param Tmax Time at which to end simulations
+##' @param record If \code{TRUE} record and return output from all
+##' time steps; otherwise just return final values
 ##' @param ... Extra parameters to give to f()
 ##' @return list containing generalised coordinates x over time and the time
 ##' @author David Sterratt
-solve.lagrange <- function(x, f, mu=1, gamma=0, dt, Tmax, ...) {
+solve.lagrange <- function(x, force, restraint, mu=1, gamma=0, dt, Tmax,
+                           record=FALSE) {
   ## Initial conditions
   x0 <- x
   xm <- x
 
+  ## Convenience constant
   epsilon <- gamma/mu/2*dt
-  xs <- matrix(NA, Tmax/dt, length(x))
-  ts <- rep(NA, Tmax/dt)
+
+  ## Space to record data
+  if (record) {
+    xs <- matrix(NA, Tmax/dt, length(x))
+    ts <- rep(NA, Tmax/dt)
+  }
   
   for (i in 1:(Tmax/dt)) {
-    xs[i,] <- x
-    ts[i] <- dt*(i-1)
-    xp <- 1/(1 + epsilon) * (f(x, ...)/mu*dt^2 + 2*x - (1 - epsilon)*xm)
+    if (record) {
+      xs[i,] <- x
+      ts[i] <- dt*(i-1)
+    }
+    
+    xp <- 1/(1 + epsilon) * (force(x)/mu*dt^2 + 2*x - (1 - epsilon)*xm)
+    ## Make sure particles lie on constraint surface
+    xp <- restraint(xp)
     xm <- x
     x <- xp
   }
-  return(list(x=xs, t=ts))
+
+  out <- list(x=x)
+  if (record) {
+    out <- c(out, list(xs=xs, ts=ts))
+  }
+  return(out)
 }
 
 spring <- function(x) {
