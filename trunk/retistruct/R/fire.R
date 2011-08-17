cc215e2a7b9c4f5ffb2f884f596204df18c410fd
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-17 14:37:52 +0000

    * Added mass and removed experement with stopping selected particles
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@697 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/fire.R
+++ trunk/retistruct/R/fire.R
@@ -1,4 +1,4 @@
-fire <- function(r, force, restraint, dt=0.1, maxmove=1E2, dtmax=10,
+fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=10,
                  Nmin=5, finc=1.1, fdec=0.5, astart=0.1, fa=0.99, a=0.1,
                  nstep=100) {
   Nsteps <- 0 
@@ -6,12 +6,9 @@ fire <- function(r, force, restraint, dt=0.1, maxmove=1E2, dtmax=10,
   v <- 0*r                              
 
   for (i in 1:nstep) {
-    f <- force(r)
-    vf <- dot(f, v)
-    ## if (all(vf > 0)) {
-    ## vf <- sum(f*v)
-    if (sum(vf) > 0) {
-      v[vf<0,] <- 0
+    f <- force(r)/m
+    vf <- sum(dot(f, v))
+    if (vf > 0) {
       v <- (1 - a)*v + a*f/vecnorm(f)*vecnorm(v)
       if (Nsteps > Nmin) {
         dt <- min(dt*finc, dtmax)
@@ -22,7 +19,7 @@ fire <- function(r, force, restraint, dt=0.1, maxmove=1E2, dtmax=10,
       Nsteps <- Nsteps + 1
     } else {
       message("Stop")
-      v[vf<0,] <- 0
+      v <- 0
       a <- astart
       dt <- dt*fdec
       Nsteps <- 0
