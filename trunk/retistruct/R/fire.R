351952c70d517773f3d891373f62bd9ab0e9ec24
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-19 09:30:34 +0000

    * RMS of F constraint for tolerance * Corrected algorithm so that m is no longer included in F
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@702 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/fire.R
+++ trunk/retistruct/R/fire.R
@@ -1,20 +1,22 @@
-fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=10,
+fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=1,
                  Nmin=5, finc=1.1, fdec=0.5, astart=0.1, fa=0.99, a=0.1,
-                 nstep=100, tol=0.02, verbose=FALSE) {
+                 nstep=100, tol=0.001, verbose=FALSE, mm=NULL) {
   Nsteps <- 0
   conv <- 1
   # Initialise velocity
   v <- 0*r                              
+  dt <- min(dt, dtmax)
 
   for (i in 1:nstep) {
-    f <- force(r)/m
-    print(max(abs(f)))
-    if (max(abs(f))<tol) {
+    f <- force(r)
+    frms <- sqrt(sum(f^2))
+    if (frms < tol) {
       conv <- 0
       break;
     }
-    vf <- sum(dot(f, v))
-    if (vf > 0) {
+    vf <- dot(f, v)
+    ## v[vf>0,] <- 0
+    if (sum(vf) > 0) {
       v <- (1 - a)*v + a*f/vecnorm(f)*vecnorm(v)
       if (Nsteps > Nmin) {
         dt <- min(dt*finc, dtmax)
@@ -29,15 +31,21 @@ fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=10,
       dt <- dt*fdec
       Nsteps <- 0
     }
-    v <- v + dt*f
+    v <- v + dt*f/m
     dr <- dt*v
     normdr <- sqrt(sum(dr*dr))
     if (normdr > maxmove) {
       if (verbose) message("Hitting maxmove limit")
       dr <- maxmove*dr/normdr
     }
+    ## if (!is.null(mm)) {
+    ##   normdr <- vecnorm(dr)
+    ##   dr[normdr>mm,] <- dr[normdr>mm,]/normdr[normdr>mm]*mm[normdr>mm]
+    ## }
     r <- r + dr
     r <- restraint(r)
   }
-  return(list(x=r, conv=conv))
+  message("Frms =", frms)
+  return(list(x=r, conv=conv, frms=frms))
 }
+
