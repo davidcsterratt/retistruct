3adc4c08123e3b995385a6268fd4db81f9951830
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-29 16:15:20 +0000

    * Fixed on x0=0.5
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@727 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/demo/retistruct.method.R
+++ trunk/retistruct/demo/retistruct.method.R
@@ -65,9 +65,10 @@ rgl.postscript("initial-projection.pdf", "pdf")
 
 ## Optimise mapping - this takes a few minutes
 alpha <- 8
-x0 <- 0.4
-r <- solve.mapping.cart(m, alpha=alpha, x0=x0, nu=1, dtmax=500, maxmove=1E3,
-                        tol=1e-5, plot.3d=FALSE)
+x0 <- 0.5
+r <- solve.mapping.cart(m, alpha=alpha, x0=x0, nu=1,
+                        dtmax=500, maxmove=1E3, tol=1e-5,
+                        plot.3d=FALSE)
 r <- optimise.mapping(r, alpha=alpha, x0=x0, nu=0.5, 
                       plot.3d=FALSE)
 
