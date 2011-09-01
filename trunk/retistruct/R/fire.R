54df92f1fb74848d084d4914404211b34a0c2d1b
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-01 13:39:18 +0000

    * Stop function functionality added
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@731 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/fire.R
+++ trunk/retistruct/R/fire.R
@@ -1,4 +1,5 @@
-fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=1,
+fire <- function(r, force, restraint, stopfun=NULL,
+                 m=1, dt=0.1, maxmove=1E2, dtmax=1,
                  Nmin=5, finc=1.1, fdec=0.5, astart=0.1, fa=0.99, a=0.1,
                  nstep=100, tol=0.00001, verbose=FALSE, mm=NULL) {
   Nsteps <- 0
@@ -6,12 +7,14 @@ fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=1,
   # Initialise velocity
   v <- 0*r                              
   dt <- min(dt, dtmax)
-
+  r0 <- r
+  
   ## Counters for number of stops and hits of dtmax and maxmove
   nstop <- 0
+  nstopfun <- 0
   ndtmax <- 0
   nmaxmove <- 0
-
+  s <- FALSE
   for (i in 1:nstep) {
     f <- force(r)
     frad <- dot(f, r/vecnorm(r))
@@ -26,7 +29,11 @@ fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=1,
     }
     vf <- dot(f, v)
     ## v[vf>0,] <- 0
-    if (sum(vf) > 0) {
+    if (!is.null(stopfun)) {
+      s <- stopfun(r)
+      if (s) print(paste("Stopfun is", s))
+    }
+    if ((sum(vf) > 0) && !s) {
       v <- (1 - a)*v + a*f/vecnorm(f)*vecnorm(v)
       if (Nsteps > Nmin) {
         dt <- min(dt*finc, dtmax)
@@ -36,7 +43,9 @@ fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=1,
       Nsteps <- Nsteps + 1
     } else {
       nstop <- nstop +1
-      v <- 0
+      v <- 0                            # Stop
+      r <- r0                           # Go back
+      nstopfun <- nstopfun + 1
       a <- astart
       dt <- dt*fdec
       Nsteps <- 0
@@ -52,11 +61,13 @@ fire <- function(r, force, restraint, m=1, dt=0.1, maxmove=1E2, dtmax=1,
     ##   normdr <- vecnorm(dr)
     ##   dr[normdr>mm,] <- dr[normdr>mm,]/normdr[normdr>mm]*mm[normdr>mm]
     ## }
+    r0 <- r
     r <- r + dr
     r <- restraint(r)
   }
   if (verbose) {
-    message(paste("FIRE: ", nstop, " stops. ",
+    message(paste("FIRE:", nstop, "stops including ",
+                  nstopfun, "stopfun stops",
                   nmaxmove, "hits of maxmove. ",
                   ndtmax, "hits of dtmax."))
     message("Frms = ", frms, "; Ftanrms = ", ftanrms)
