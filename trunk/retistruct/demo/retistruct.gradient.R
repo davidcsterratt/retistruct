3e1f90252d8280330a619332388351466320a27d
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-28 14:52:36 +0000

    * Created ChenEtal95 demo
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@764 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/retistruct/demo/retistruct.gradient.R
@@ -0,0 +1,24 @@
+## Load the raw data
+dataset <- file.path(system.file(package = "retistruct"), "extdata", "ChenEtal95")
+o <- retistruct.read.dataset(dataset)
+
+## Load the human annotation of tears
+o <- retistruct.read.markup(o)
+
+## Initial plot
+## plot.flat(o)
+
+## Reconstruct
+r <- retistruct.reconstruct(o)
+
+## Plot with gridlines
+options(outline.col="yellow")
+options(grid.maj.col="yellow")
+options(grid.min.col="white")
+par(mar=c(0.1,0.1,0.1,0.1))
+par(mfrow=c(1, 2))
+plot.flat(r, mesh=FALSE, stitch=FALSE, markup=FALSE)
+plot.polar(r, mesh=FALSE)
+
+## To print for PloS Biol.
+## dev.copy2eps(file="ChenEtal95.eps", width=6.83, height=6.83/2)
