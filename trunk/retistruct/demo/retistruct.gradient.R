842463a232698f1d8ad34f5c57c546547de53467
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-05 15:23:10 +0000

    * Presentation tweaks
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@768 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/demo/retistruct.gradient.R
+++ trunk/retistruct/demo/retistruct.gradient.R
@@ -12,10 +12,12 @@ o <- retistruct.read.markup(o)
 r <- retistruct.reconstruct(o)
 
 ## Plot with gridlines
-par(mar=c(0.1,0.1,0.1,0.1))
+par(mar=c(0.2,0.2,0.2,0.2))
 par(mfrow=c(1, 2))
 plot.flat(r, mesh=FALSE, stitch=FALSE, markup=FALSE)
+mtext("A", adj=0, font=2, line=-0.9)
 plot.polar(r, mesh=FALSE)
+mtext("B", adj=0, font=2, line=-0.9)
 
 ## To print for PloS Biol.
 ## dev.copy2eps(file="ChenEtal95.eps", width=6.83, height=6.83/2)
