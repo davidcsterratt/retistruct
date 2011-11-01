6ae217f76d028ad1db5d65172a4331594f89e430
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-01 16:48:22 +0000

    * Subtle change to scalebar logic
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@843 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/Outline.R
+++ trunk/retistruct/R/Outline.R
@@ -29,9 +29,11 @@ Outline <- function(P, scale=NA, im=NULL) {
 }
 
 ##' Plot flat outline. If the optional argument \code{plot.image} is
-##' \code{TRUE} the image is displayed behind the outlin. If the
-##' optional argument \code{scalebar} is present and numeric, a scale
-##' bar of length \code{scalebar} mm is plotted.
+##' \code{TRUE} the image (if it is present) is displayed behind the
+##' outline. If the optional argument \code{scalebar} is present and
+##' numeric, a scale bar of length \code{scalebar} mm is plotted. If
+##' \code{scalebar} is \code{FALSE} the scale bar is supressed, and by
+##' default a scale bar of 1mm is drawn.
 ##'
 ##' @title Flat plot of outline
 ##' @param o \code{Outline} object
@@ -43,8 +45,10 @@ Outline <- function(P, scale=NA, im=NULL) {
 plot.flat.outline <- function(o, axt="n", ylim=NULL, ...) {
   args <- list(...)
   plot.image <- is.null(args$image) || args$image
-  scalebar   <- ifelse(!is.null(args$scalebar) && !is.null(o$scale),
-                       args$scalebar, FALSE)
+  scalebar <- ifelse(!is.null(args$scalebar),
+                     args$scalebar, 1) # Default value of scalebar 1mm
+  scalebar <- ifelse(is.numeric(scalebar) && !is.null(o$scale), scalebar, FALSE)
+
   with(o, {
     s <- which(!is.na(gb))                # source index
     d <- na.omit(gb)                      # destination index
