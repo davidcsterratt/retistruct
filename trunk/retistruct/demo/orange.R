53247fd1eca7ac430fd4d8d5dfad8fb9ff8c761c
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-01 16:05:50 +0000

    * Tidied out data directory
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@842 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/demo/orange.R
+++ trunk/retistruct/demo/orange.R
@@ -1,50 +1,21 @@
-library(retistruct)
-library(pixmap)
-
-im <- read.pnm("../data/20091214191056-small.ppm")
-# plot(im)
-
 ## Read in data
-P.raw <- read.table("../data/20091214191056-small.txt")
-P <- cbind(P.raw[,1], 520-P.raw[,2])
+dataset <- file.path(system.file(package = "retistruct"), "extdata", "orange")
+o <- retistruct.read.dataset(dataset)
 
-tearmat <- rbind(c(5,   1, 11),
-                 c(21, 16, 32),
-                 c(29, 25, 31),
-                 c(52, 39, 65))
-colnames(tearmat) <- c("apex", "end1", "end2")
+## Load the human annotation of tears
+o <- retistruct.read.markup(o)
 
+## Initial plot
+par(mar=c(0.1,0.1,0.1,0.1))
+plot.flat(o)
 
-## Stitching
-s <- stitch.retina(P, tearmat)
-plot.stitch(s)
-dev.print(pdf, file="../figures/orange-stitched.pdf", width=5)
+## Reconstruct
+r <- retistruct.reconstruct(o)
 
-t <- make.triangulation(s, d=30)
-with(t, trimesh(T, P, col="black", add=TRUE))
-dev.print(pdf, file="../figures/orange-stitched-triangulated.pdf", width=5)
-
-m <- merge.points(t, s)
-## Plot stiched retina in 2D (messy)
-## trimesh(Tt, Pt, col="black")
-
-## Plotting
-plot(P)
-with(s, plot.outline(P, gb))
-dev.print(pdf, file="../figures/orange-outline.pdf", width=5)
-
-p <- project.to.sphere(m, t, phi0=45*pi/180)
-rgl.postscript(file="orange-projected.pdf", fmt="pdf")
+plot.flat(r, mesh=FALSE, stitch=FALSE, markup=FALSE)
 
 ## Initial plot in 3D space
-plot.retina(p$phi, p$lambda, p$R, m$Tt, m$Rsett)
-
-## Optimisation
-f <- solve.mapping(p, m, t, s, dt=1E-5, nstep=2000)
-
-plot(im)
-dev.print(png, file="../figures/orange-no-grid.png", width=500)
-plot.gridlines.flat(t$P, t$T, f$phi, f$lambda, m$Tt, p$phi0,
-                                Phis=(-1:2)*pi/4, Lambdas=(0:1)*pi/2, lwd=3)
-
-dev.print(png, file="../figures/orange-grid.png", width=5)
+## plot.retina(p$phi, p$lambda, p$R, m$Tt, m$Rsett)
+## dev.print(pdf, file="../figures/orange-outline.pdf", width=5)
+## dev.print(png, file="../figures/orange-no-grid.png", width=500)
+## dev.print(png, file="../figures/orange-grid.png", width=5)
