270a9c87b9b5d8ebdbdb9981284166def577808f
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-02 11:59:47 +0000

    * Initial revision
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@847 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/retistruct/R/RetinalReconstructedDataset.R
@@ -0,0 +1,128 @@
+##' Create an object that is specific to retinal datasets. This
+##' contains methods that return datapoint and landmark coordinates
+##' that have been transformed according to the values of
+##' \code{DVflip} and \code{side}.
+##'
+##' @title RetinalReonstructedDataset constructor
+##' @param r Object that inherits both \code{reconstructedDataset} and
+##' \code{dataset}.
+##' @return \code{retinalReonstructedDataset} object. This does not
+##' contain any extra fields, but there are extra mthods dthat apply
+##' to it.
+##' @author David Sterratt
+RetinalReconstructedDataset <- function(r) {
+  if (!(inherits(r, "reconstructedDataset") &
+        inherits(r, "retinalDataset"))) {
+    stop("Argument needs to inherit reconstructedDataset and retinalDataset")
+  }
+  class(r) <- unique(c("retinalReconstructedDataset", class(r)))
+  return(r)
+}
+
+##' Get spherical coordinates of datapoints, transformed according to
+##' the values of \code{DVflip} and \code{side}.
+##'
+##' @title Get transformed spherical coordinates of datapoints
+##' @param r \code{retinalReonstructedDataset} object.
+##' @return \code{Dss}
+##' @method getDss retinalReconstructedDataset
+##' @author David Sterratt
+getDss.retinalReconstructedDataset <- function(r) {
+  Dss <- NextMethod()
+  if (r$DVflip) {
+    for (i in 1:length(Dss)) {
+      Dss[[i]][,"lambda"] <- -Dss[[i]][,"lambda"]
+    }
+  }
+  if (r$side=="Left") {
+    for (i in 1:length(Dss)) {
+      Dss[[i]][,"lambda"] <- pi - Dss[[i]][,"lambda"]
+    }
+  }
+  return(Dss)
+}
+
+##' Get Karcher mean of datapoints in spherical coordinates,
+##' transformed according to the values of \code{DVflip} and
+##' \code{side}.
+##'
+##' @title Get transformed spherical coordinates of Karcher mean of
+##' datapoints
+##' @param r \code{retinalReonstructedDataset} object.
+##' @return \code{Dss.mean}
+##' @method getDss.mean retinalReconstructedDataset
+##' @author David Sterratt
+getDss.mean.retinalReconstructedDataset <- function(r) {
+  Dss.mean <- NextMethod()
+  Dss.mean[["OD"]] <- NULL
+  if (r$DVflip) {
+    for (i in 1:length(Dss.mean)) {
+      Dss.mean[[i]]["lambda"] <- -Dss.mean[[i]]["lambda"]
+    }
+  }
+  if (r$side=="Left") {
+    for (i in 1:length(Dss.mean)) {
+      Dss.mean[[i]]["lambda"] <- pi - Dss.mean[[i]]["lambda"]
+    }
+  }
+  return(Dss.mean)
+}
+
+##' Get spherical coordinates of landmarks, transformed according to
+##' the values of \code{DVflip} and \code{side}.
+##'
+##' @title Get transformed spherical coordinates of datapoints
+##' @param r \code{retinalReonstructedDataset} object.
+##' @return \code{Dss}
+##' @method getDss retinalReconstructedDataset
+##' @author David Sterratt
+getSss.retinalReconstructedDataset <- function(r) {
+  Sss <- NextMethod()
+  if (r$DVflip) {
+    for (i in 1:length(Sss)) {
+      Sss[[i]][,"lambda"] <- -Sss[[i]][,"lambda"]
+    }
+  }
+  if (r$side=="Left") {
+    for (i in 1:length(Sss)) {
+      Sss[[i]][,"lambda"] <- pi - Sss[[i]][,"lambda"]
+    }
+  }
+  return(Sss)
+}
+
+##' Get spherical coordinates of datapoints, transformed according to
+##' the values of \code{DVflip} and \code{side}.
+##'
+##' @title Get transformed spherical coordinates of datapoints
+##' @param r \code{retinalReonstructedDataset} object.
+##' @return \code{Dss}
+##' @method getDss retinalReconstructedDataset
+##' @author David Sterratt
+getTss.retinalReconstructedDataset <- function(r) {
+  Tss <- NextMethod()
+  if (r$DVflip) {
+    for (i in 1:length(Tss)) {
+      Tss[[i]][,"lambda"] <- -Tss[[i]][,"lambda"]
+    }
+  }
+  if (r$side=="Left") {
+    for (i in 1:length(Tss)) {
+      Tss[[i]][,"lambda"] <- pi - Tss[[i]][,"lambda"]
+    }
+  }
+  return(Tss)
+}
+
+plot.polar.retinalReconstructedDataset <- function(r, show.grid=TRUE,
+                                                   grid.col="gray",
+                                                   grid.bg="transparent", 
+                                                   grid.int.minor=15,
+                                                   grid.int.major=45,
+                                                   flip.horiz=FALSE, ...) {
+  ## This will call plot.polar.reconstructedDataset()
+  NextMethod(flip.horiz=(r$side=="Left"),
+             labels=c("N", "D", "T", "V"))
+}
+
+
