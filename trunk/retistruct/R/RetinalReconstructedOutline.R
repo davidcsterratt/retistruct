5cf1e188ae5b62b4dfffb035c13cdecadddce7a5
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-03 10:40:22 +0000

    * Initial revision
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@855 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/retistruct/R/RetinalReconstructedOutline.R
@@ -0,0 +1,34 @@
+##' Create an object that is specific to retinal datasets. This
+##' contains methods that return datapoint and landmark coordinates
+##' that have been transformed according to the values of
+##' \code{DVflip} and \code{side}.
+##'
+##' @title RetinalReonstructedOutline constructor
+##' @param r Object that inherits both \code{reconstructedDataset} and
+##' \code{dataset}.
+##' @return \code{retinalReonstructedDataset} object. This does not
+##' contain any extra fields, but there are extra mthods dthat apply
+##' to it.
+##' @author David Sterratt
+RetinalReconstructedOutline <- function(r) {
+  if (!(inherits(r, "reconstructedOutline"))) {
+    stop("Argument needs to inherit reconstructedOutline")
+  }
+  class(r) <- addClass("retinalReconstructedOutline", r)
+  return(r)
+}
+
+getIms.retinalReconstructedOutline <- function(r) {
+  ims <- NextMethod()
+  if (r$DVflip) {
+    if (!is.null(ims)) {
+      ims[,"lambda"] <- -ims[,"lambda"]
+    }
+  }
+  if (r$side=="Left") {
+    if (!is.null(ims)) {
+      ims[,"lambda"] <- 2*pi - ims[,"lambda"]
+    }
+  }
+  return(ims)
+}
