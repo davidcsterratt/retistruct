e7634b493aba3eb69d0caae9f6b07f6ce29ca2c8
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-11 17:26:02 +0000

    * Improved documentation of line2bary()
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@900 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/R/tsearch.R
+++ trunk/retistruct/R/tsearch.R
@@ -1,50 +1,43 @@
-bary2cart <- function(T, Beta) {
-  ## Conversion of Barycentric to Cartesian coordinates.
-  ## Given a reference simplex T in N dimensions represented by a
-  ## (N+1)-by-(N) matrix, and arbitrary point Beta in baryocentric coordinates,
-  ## represented by a N+1-by-1 row vector, the cartesian coordinates P are
-  ## given
-  ##
-  ## P = Beta * T
-  return(Beta %*% T)
-}
-
+##' Given a reference triangle in embedded in a 3D space, represented
+##' by a 3-by-3 matrix, and arbitrary point P in cartesion
+##' coordinates, represented by a 3-by-1 row vector, the intersection
+##' between a line in the direction of P, from the origin and the
+##' simplex can be can be written as
+##'
+##' rho * P = Beta * T
+##'
+##' where Beta is a N vector of the barycentric coordinates and rho
+##' scales P. A criteria on Beta is that
+##'
+##' sum (Beta) == 1
+##'
+##' and therefore we can write the above as
+##'
+##' beta' = [beta[1], beta[2], rho]
+##'
+##' T' = [T[1,] - T[3,]; T[2,] - T[3,]; P]
+##'
+##' -T[3,] = beta' * T'
+##'
+##' beta' = -T[3,] * solve(T')
+##' 
+##' rho * P - T(end, :) = Beta(1:end-1) * (T(1:end-1,:) - ones(N,1) * T(end,:))
+##'
+##' and then we can solve for Beta as
+##'
+##' Beta(1:end-1) = (P - T(end,:)) / (T(1:end-1,:) - ones(N,1) * T(end,:))
+##' Beta(end) = sum(Beta)
+##' @title Conversion of the intersection of a line with triangle  to Barycentric coordinates
+##' @param T 3-by-3 matrix representing triangle in 3D space
+##' @param P Direction of line from the origin
+##' @return Intersection of line with triangle expressed in
+##' barycentric coordinates with respect to the triangle
+##' @author David Sterratt
 line2bary <- function(T, P) {
-  ## Conversion of the intersection of a cartesian line with triangle
-  ## in 3D to Barycentric coordinates.  Given a reference triangle in
-  ## embedded in a 3D space, represented by a 3-by-3 matrix, and
-  ## arbitrary point P in cartesion coordinates, represented by a
-  ## 3-by-1 row vector, the intersection between a line in the
-  ## direction of P, from the origin and the simplex can be can be
-  ## written as
-  ##
-  ## rho * P = Beta * T
-  ##
-  ## where Beta is a N vector of the barycentric coordinates and rho
-  ## scales P. A criteria on Beta is that
-  ##
-  ## sum (Beta) == 1
-  ##
-  ## and therefore we can write the above as
-  ##
-  ## beta' = [beta[1], beta[2], rho]
-  ##
-  ## T' = [T[1,] - T[3,]; T[2,] - T[3,]; P]
-  ##
-  ## -T[3,] = beta' * T'
-  ##
-  ## beta' = -T[3,] * solve(T')
-  ## 
-  ## rho * P - T(end, :) = Beta(1:end-1) * (T(1:end-1,:) - ones(N,1) * T(end,:))
-  ##
-  ## and then we can solve for Beta as
-  ##
-  ## Beta(1:end-1) = (P - T(end,:)) / (T(1:end-1,:) - ones(N,1) * T(end,:))
-  ## Beta(end) = sum(Beta)
-  ##
+
   M <- dim(P)[1]
   N <- dim(P)[2]
-  ##
+
   Tp <- rbind(T[1,] - T[3,], T[2,] - T[3,], -P)
   Beta <- -T[3,] %*% solve(Tp)
   Beta <- rbind(c(Beta[c(1,2)], 1 - Beta[1] - Beta[2], Beta[3]))
@@ -52,10 +45,6 @@ line2bary <- function(T, P) {
 }
 
 tsearch.sphere <- function(x, t, xi) {
-##  if (nargin != 3)
-##    print_usage ();
-##  endif
-
   nt <- dim(t)[1]
   m <- dim(x)[1]
   n <- dim(x)[2]
