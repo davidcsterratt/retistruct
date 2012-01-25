be5818509b1f19edc9cb1d595066187cd2ebc537
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-25 14:06:50 +0000

    * Removed titration
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@928 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/demo/good.bad.R
+++ trunk/retistruct/demo/good.bad.R
@@ -49,11 +49,3 @@ mtext("H", adj=0, font=2, line=-0.9)
 ## Printing
 ## dev.copy2pdf(file="retistruct-good-bad.pdf", width=6.83, height=6.83/2)
 
-## Titrate to give impression of whether reconstruction is good or bad
-t.good <- titrate.ReconstructedOutline(r.good)
-
-par(mar=c(3.5, 3.5, 0.5, 0.5), mgp=c(1.7, 0.5, 0), tcl=-0.2)
-with(t.good$dat, plot(phi0d, sqrt.E, type="l", xlab=expression(paste(italic(phi)[0], " (degrees)")), ylab=expression(sqrt(italic(E)[L]))))
-with(t.good,
-     points(phi0d.orig, dat[which(dat$phi0d==phi0d.orig),"sqrt.E"], col="red"))
-
