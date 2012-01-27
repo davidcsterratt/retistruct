c8452624849e111c85aee60d32f3d71d331c4612
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-27 17:16:33 +0000

    * Install version of geometry from R-Forge
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@936 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/install.R
+++ trunk/install.R
@@ -8,7 +8,7 @@ options(repos="http://cran.r-project.org")
 ## It ought to be possible to get these dependendcies from the
 ## retistruct DESCRIPTION, but I don't know how!!
 if (!require("foreign"))       install.packages("foreign")
-if (!require("geometry"))      install.packages("geometry")
+if (!require("geometry"))      install.packages("geometry", repos="http://R-Forge.R-project.org")
 if (!require("gWidgets"))      install.packages("gWidgets")
 if (!require("gWidgetsRGtk2")) install.packages("gWidgetsRGtk2")
 if (!require("cairoDevice"))   install.packages("cairoDevice")
@@ -22,7 +22,7 @@ if (!require("Triangle"))      install.packages("Triangle", repos="http://R-Forg
 update.packages(Sys.getenv("R_LIBS_USER"))
 ## These lines needed while  Triangle and geometry only on R-Forge
 install.packages("Triangle", repos="http://R-Forge.R-project.org")
-## install.packages("geometry", repos="http://R-Forge.R-project.org")
+install.packages("geometry", repos="http://R-Forge.R-project.org")
 ## Now install retistruct
 install.packages("retistruct_0.4.7.tar.gz")
 install.packages("retistructgui_0.4.7.tar.gz")
