b0ff8a84155ac9f7cd9063c19ec1b6fa95de8d7c
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-22 12:08:24 +0000

    * ray.arc.intersection() now detects whether ray is truly intersecting   arc and reports only first intersection
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@989 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/experimental/ray-tracing.R
+++ trunk/experimental/ray-tracing.R
@@ -1,13 +1,14 @@
 source("../retistruct/R/geometry.R")
 
-##' @title Compute intersection of ray and cirle
-##' @param Ptheta0 Vector comprising cartesian coordinates of incident
+##' @title Compute intersection of ray and arc
+##' @param Ptheta Vector comprising cartesian coordinates of incident
 ##' ray origin and angle with x-axis of incident ray
 ##' @param A Left-edge of circle on x-axis
 ##' @param R Radius of circle
-##' @return 
+##' @param C Angle at which arc cut off
+##' @return Vector of x, y and lambda
 ##' @author David Sterratt
-ray.lens.intersection <- function(Ptheta, A, R) {
+ray.arc.intersection <- function(Ptheta, A, R, C) {
   P <- Ptheta[1:2]
   theta <- Ptheta[3]
   X <- A + R                           # X-co-ord of centre of circle
@@ -15,14 +16,39 @@ ray.lens.intersection <- function(Ptheta, A, R) {
   y <- P[2]
 
   ## Solve quadratic
-
   bh <- (x - X)*cos(theta) + y*sin(theta)
   ac <- ((x - X)^2 + y^2 - R^2)
+
+  ## The ray doesn't intersect the circle at all
   if ((bh^2 - ac) < 0) {
     return(NA)
   }
-  lambda <- -bh -sign(R)*sqrt(bh^2 - ac)
-  return(c(x + lambda*cos(theta), y + lambda*sin(theta)))
+
+  ## The ray does intersect the circle; compute the distances along
+  ## the ray and the intersection points
+  lambda <- c(-bh - sign(R)*sqrt(bh^2 - ac),
+              -bh + sign(R)*sqrt(bh^2 - ac))
+  x <- x + lambda*cos(theta)
+  y <- y + lambda*sin(theta)
+
+  ## Find out if the intersections are on the arc
+  gamma <- acos((X - x)/R)
+  print(gamma)
+  print(C)
+  ## If not, return NA
+  if (all(gamma > C)) {
+    return(NA)
+  }
+  out <- cbind(x, y, lambda)
+  print(out)
+  out <- out[gamma <= C,,drop=FALSE]
+  ## print(out)
+  out <- out[out[,"lambda"]>0,,drop=FALSE]
+  out <- out[which.min(out[,"lambda"]),]
+  print(out)
+  
+  ## Otherwise return the first intersection
+  return(out)
 }
 
 ##' @title Compute origin and deflection of new ray
@@ -34,11 +60,11 @@ ray.lens.intersection <- function(Ptheta, A, R) {
 ##' @param n1 Refractive index of medium to right of circle
 ##' @return Vector comprinsing new origin and angle
 ##' @author David Sterratt
-new.ray <- function(Ptheta0, A1, R1, n0, n1) {
+new.ray <- function(Ptheta0, A1, R1, C1, n0, n1) {
   P0 <- Ptheta0[1:2]
   theta0 <- Ptheta0[3]
   ## Compute intersection
-  P1 <- ray.lens.intersection(Ptheta0, A1, R1)
+  P1 <- ray.arc.intersection(Ptheta0, A1, R1, C1)
   if (any(is.na(P1))) {
     return(NA)
   }
@@ -48,7 +74,7 @@ new.ray <- function(Ptheta0, A1, R1, n0, n1) {
   ## Snell's law
   I1 <- asin(n0*sin(I0)/n1)
   theta1 <- I1 - alpha1
-  return(c(P1, theta1))
+  return(c(P1[1:2], theta1))
 }
 
 ##' @title Draw a lens
@@ -76,7 +102,7 @@ trace.ray <- function(r0, S) {
   r <- matrix(r0, ncol=3)
   with(S, {
     for (i in 1:(nrow(S))) {
-      r1 <- new.ray(r0, A[i], R[i], n[i], n[i+1])
+      r1 <- new.ray(r0, A[i], R[i], C[i], n[i], n[i+1])
       if (!is.na(r1[1])) {
         r <- rbind(r, r1)
         r0 <- r1
@@ -105,10 +131,12 @@ draw.system <- function(S) {
 ## radii of surfaces (R) and refracive indices (n).
 S <- data.frame(A=c(3,  4,  8),
                 R=c(1, -1, -4),
+                C=c(pi/2, pi/2, pi/2),
                 n=c(1, 1.4, 1))
 
 S.hughes <- data.frame(A=c(0,     0.26,  0.881, 1.778,  3.695,  4.591,  5.981),
                        R=c(2.965, 2.705, 2.340, 0.958, -0.958, -2.340, -2.813),
+                       C=c( pi/2,  pi/2,  pi/2,  pi/2,  pi/2,   pi/2,   pi/2),
                        n=c(1,     1.38,  1.337, 1.390,  1.50,   1.390,  1.337))
 
 S <- S.hughes
@@ -121,15 +149,18 @@ draw.system(S)
 ## r <- trace.ray(c(-500, 0.5, -0.001), S)
 ## r <- trace.ray(c(-500, 0.5, -0), S)
 
-r <- trace.ray(c(-500, 10, -0.02), S)
+## r <- trace.ray(c(-500, 10, -0.02), S)
 
-r <- trace.ray(c(-500, 50, -0.1), S)
-r <- trace.ray(c(-500, 50.5, -0.1), S)
+## r <- trace.ray(c(-500, 50, -0.1), S)
+## r <- trace.ray(c(-500, 50.5, -0.1), S)
 
 r <- trace.ray(c(-500, 101, -0.2), S)
-r <- trace.ray(c(-500, 101.5, -0.2), S)
+## r <- trace.ray(c(-500, 101.5, -0.2), S)
+r <- trace.ray(c(-500, 102.5, -0.2), S)
 r <- trace.ray(c(-500, 102, -0.2), S)
-r <- trace.ray(c(-500, 103, -0.2), S)
+r <- trace.ray(c(-500, 100, -0.2), S)
+## r <- trace.ray(c(-500, 100.8, -0.2), S)
+## r <- trace.ray(c(-500, 99, -0.2), S)
 
 r <- trace.ray(c(1, 4, -pi/2), S)
 
