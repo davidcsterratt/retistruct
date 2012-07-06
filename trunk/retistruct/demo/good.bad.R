b043a6077cd5601427b14839262c9d37b72048b2
sterratt@INF.ED.AC.UK <sterratt@INF.ED.AC.UK@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-06 15:13:28 +0000

    * Rename good and bad, low and high
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@1146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/retistruct/demo/good.bad.R
+++ trunk/retistruct/demo/good.bad.R
@@ -1,18 +1,18 @@
 ## Load the good raw data
 dataset <- file.path(system.file(package = "retistruct"), "extdata", "GMB530/R-CONTRA")
-r.good <- retistruct.read.dataset(dataset)
+r.low <- retistruct.read.dataset(dataset)
 ## Load the human annotation of tears
-r.good <- retistruct.read.markup(r.good)
+r.low <- retistruct.read.markup(r.low)
 ## Reconstruct
-r.good <- retistruct.reconstruct(r.good)
+r.low <- retistruct.reconstruct(r.low)
 
 ## Load the bad raw data
 dataset <- file.path(system.file(package = "retistruct"), "extdata", "GM182-4/R-CONTRA")
-r.bad <- retistruct.read.dataset(dataset)
+r.high <- retistruct.read.dataset(dataset)
 ## Load the human annotation of tears
-r.bad <- retistruct.read.markup(r.bad)
+r.high <- retistruct.read.markup(r.high)
 ## Reconstruct
-r.bad <- retistruct.reconstruct(r.bad)
+r.high <- retistruct.reconstruct(r.high)
 
 ## Plotting
 x11(width=6.83, height=6.83/2)
@@ -22,34 +22,34 @@ par(mfrow=c(2, 4))
 par(mar=c(2.2, 2.2, 0.5, 0.5),
     mgp=c(1.1, 0.2, 0),
     tcl=-0.15)
-lvsLplot(r.good)
+lvsLplot(r.low)
 panlabel("A")
 
 par(mar=c(0.5, 0.5, 0.5, 0.5))
-flatplot(r.good, strain=TRUE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=FALSE, landmarks=FALSE)
+flatplot(r.low, strain=TRUE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=FALSE, landmarks=FALSE)
 panlabel("B")
 
-flatplot(r.good, strain=FALSE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=TRUE)
+flatplot(r.low, strain=FALSE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=TRUE)
 panlabel("C")
 
-projection(r.good, datapoints=FALSE, datapoint.means=FALSE, datapoint.contours=FALSE)
+projection(r.low, datapoints=FALSE, datapoint.means=FALSE, datapoint.contours=FALSE)
 panlabel("D")
 
 ## Bad Retina
 par(mar=c(2.2, 2.2, 0.5, 0.5))
-lvsLplot(r.bad)
+lvsLplot(r.high)
 panlabel("E")
 
 par(mar=c(0.5, 0.5, 0.5, 0.5))
-flatplot(r.bad, strain=TRUE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=FALSE, landmarks=FALSE)
+flatplot(r.high, strain=TRUE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=FALSE, landmarks=FALSE)
 panlabel("F")
 
-flatplot(r.bad, strain=FALSE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=TRUE)
+flatplot(r.high, strain=FALSE, mesh=FALSE, stitch=FALSE, markup=FALSE, datapoints=FALSE, grid=TRUE)
 panlabel("G")
 
-projection(r.bad, datapoints=FALSE, datapoint.means=FALSE, datapoint.contours=FALSE)
+projection(r.high, datapoints=FALSE, datapoint.means=FALSE, datapoint.contours=FALSE)
 panlabel("H")
 
 ## Printing
-## dev.copy2eps(file="fig2-retistruct-good-bad.eps", width=6.83, height=6.83/2)
+## dev.print(svg, file="fig2-retistruct-low-high.svg", width=6.83, height=6.83/2)
 
