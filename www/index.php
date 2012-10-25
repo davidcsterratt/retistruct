d76f34a81b9db843011bfcb6daa4b64dab0ccd22
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-25 11:48:47 +0000

    * Initial version of website
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -6,10 +6,9 @@
 
 $domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
 $group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='r-forge.r-project.org/themes/rforge/';
+$themeroot='r-forge.r-project.org/themes/rforge/'; ?>
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -17,35 +16,72 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="http://<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<title>Retistruct</title>
   </head>
 
 <body>
 
-<!-- R-Forge Logo -->
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="http://r-forge.r-project.org/"><img src="http://<?php echo $themeroot; ?>/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
-
-
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<img src="folding-small.png" style="float: left"><h1>Retistruct</h1>
+<p class="slogan">-- computational reconstruction and transformation of flattened retinae</p>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p style="clear: both">Retistruct is
+ an <a href="http://www.r-project.org">R</a> package to morph a flat
+ surface with cuts (a dissected flat-mount retina) onto a
+ curvilinear surface (the a standard retinal shape).  It can estimate
+ the position of a point on the intact adult retina to within
+ 8&deg; of arc
+ (3.6% of nasotemporal axis). The coordinates in reconstructed retinae
+ can be transformed to visuotopic coordinates.
+ </p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<h2>How Retistruct works</h2>
+
+<img src="retistruct.small.png" style="float: right"><p>Reconstruction
+is achieved by: stitching the marked-up cuts of the flat-mount
+outline; dividing the stitched outline into a mesh whose vertices then
+are mapped onto a curtailed sphere; and finally moving the vertices so
+as to minimise a physically-inspired deformation energy function. <ul>
+      <li><a href="2012-09-neuroinf.pdf">This poster</a>
+and <a href="http://www.youtube.com/watch?v=LpuqXo8NEOo">this YouTube
+video</a>, presented at the 2012 Neuroinformatics Meeting in Munich,
+has more details and examples of reconstructions and projections into
+visual space. </li>
+      <li>A paper will be forthcoming.</li>
+      </ul></p>
+
+<h2>Download</h2>
+
+<p><a href="http://www.neuralmapformation.org/retistruct_0.5.5.zip">This
+    zip file contains version 0.5.5, including a user manual with
+    full installation instructions.</a></p>
+
+<!--<p> The <strong>project summary page</strong> you can --
+  --find <a href="http://<?php echo $domain; ?>/projects/<?php echo --
+  --$group_name; ?>/"><strong>here</strong></a>. </p> -->
+
+<h2>Authors and funding</h2>
+
+<p>Retistruct was written
+  by <a href="http://homepages.inf.ed.ac.uk/sterratt/">David
+  Sterratt</a> at the <a href="http://www.ed.ac.uk/">University of
+  Edinburgh</a>, and tested by Daniel Lyngholm and Ian Thompson at the
+  <a href="http://www.kcl.ac.uk/depsta/biomedical/mrc/">MRC Centre for
+  Developmental Neurobiology, KCL</a>.
+ </p>
+
+<p>This work was supported by a Programme Grant from
+the <a href="http://www.wellcome.ac.uk">Wellcome Trust</a> (G083305).
+</p>
+
+
 
 </body>
 </html>
+
+<!--  LocalWords:  Retistruct YouTube Sterratt Lyngholm MRC KCL
+ -->
