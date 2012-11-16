2456f1acaa7c4aba352c76c4622918de2b43faed
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-16 14:59:26 +0000

    * Issue #625: Demo to reproduce figure 6
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@1223 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/retistructdemos/demo/figure6.R
+++ pkg/retistructdemos/demo/figure6.R
@@ -2,6 +2,9 @@ library(retistruct)
 ## Reconstruct left contra
 root <- system.file(package = "retistructdemos")
 
+options(contour.levels=c(25, 50, 75, 95))
+
+if (1) {
 dataset <- file.path(root, "extdata", "Figure_6-data", "left-contra")
 r <- retistruct.read.dataset(dataset)
 r <- retistruct.read.markup(r)
@@ -24,32 +27,80 @@ dataset <- file.path(root, "extdata", "Figure_6-data", "right-ipsi")
 r <- retistruct.read.dataset(dataset)
 r <- retistruct.read.markup(r)
 r.ri <- retistruct.reconstruct(r)
+}
 
 width <- 6.83
-height <- width/2.5
+height <- width*2/3
 ## par(mfrow=c(2, 4))
 par(mar=c(0.5, 0.5, 1.5, 0.5))
-layout(matrix(1:8, 2, 4, byrow=TRUE), widths=c(1, 2, 2, 1))
-## Right Contra
-projection(r.rc)
-projection(r.rc, projection=sinusoidal, transform=invert.sphere, axisdir=cbind(phi=145, lambda=30))
-title("Right Contra")
+layout(rbind(c(1, 2, 5, 6),
+             c(3, 4, 5, 6),
+             c(0, 0, 7, 7)),
+       widths =c(1, 1, 2, 2),
+       heights=c(1, 1, 2))
 
-## Left Contra
-projection(r.lc, projection=sinusoidal, transform=invert.sphere, axisdir=cbind(phi=145, lambda=150))
-title("Left Contra")
-projection(r.lc)
-
-
-## Right Ipsi
-projection(r.ri)
-projection(r.ri, projection=sinusoidal, transform=invert.sphere, axisdir=cbind(phi=145, lambda=30))
+## 1. Right Ipsi
+projection(r.ri, image=FALSE, cex=0.1)
 title("Right Ipsi")
 
-
-## Left Ipsi
-projection(r.li, projection=sinusoidal, transform=invert.sphere, axisdir=cbind(phi=145, lambda=150))
+## 2. Left Ipsi
+projection(r.li, image=FALSE, cex=0.1)
 title("Left Ipsi")
-projection(r.li)
+
+## 3. Right Contra
+projection(r.rc, image=FALSE, cex=0.1)
+title("Right Contra")
+
+## 4. Left Contra
+projection(r.lc, image=FALSE, cex=0.1)
+title("Left Contra")
+
+## 5. Ipsi orthographic correct
+projection(r.li, projection=orthographic,
+           transform=invert.sphere.to.hemisphere,
+           axisdir=cbind(phi=22, lambda=64),
+           proj.centre=cbind(phi=50, lambda=0),
+           image=FALSE, pole=TRUE)
+projection(r.ri, projection=orthographic,
+           transform=invert.sphere.to.hemisphere,
+           axisdir=cbind(phi=22, lambda=-64),
+           proj.centre=cbind(phi=50, lambda=0),
+           image=FALSE, pole=TRUE,
+           add=TRUE)
+title(expression(paste("Ipsi: O.A. at ", 22 * degree, " el., ", 64 * degree, " az.")))
+
+## 6. Ipsi orthographic incorrect
+projection(r.li, projection=orthographic,
+           transform=invert.sphere.to.hemisphere,
+           axisdir=cbind(phi=35, lambda=60),
+           proj.centre=cbind(phi=50, lambda=0),
+           image=FALSE, pole=TRUE)
+projection(r.ri, projection=orthographic,
+           transform=invert.sphere.to.hemisphere,
+           axisdir=cbind(phi=35, lambda=-60),
+           proj.centre=cbind(phi=50, lambda=0),
+           image=FALSE, pole=TRUE,
+           add=TRUE)
+title(expression(paste("Ipsi: O.A. at ", 35 * degree, " el., ", 60 * degree, " az.")))
+
+
+## 7. Contra Sinusoidal correct
+projection(r.lc, projection=sinusoidal,
+           transform=invert.sphere.to.hemisphere,
+           axisdir=cbind(phi=22, lambda=64),
+           proj.centre=cbind(phi=0, lambda=0),
+           cex=0.1,
+           image=FALSE, pole=TRUE)
+projection(r.rc, projection=sinusoidal,
+           transform=invert.sphere.to.hemisphere,
+           axisdir=cbind(phi=22, lambda=-64),
+           proj.centre=cbind(phi=0, lambda=0),
+           cex=0.1,
+           image=FALSE, pole=TRUE,
+           add=TRUE)
+title(expression(paste("Contra: O.A. at ", 22 * degree, " el., ", 64 * degree, " az.")))
+
 
 dev.print(pdf, "figure6.pdf", width=width, height=height)
+
+
