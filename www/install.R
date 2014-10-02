1839f964c78e50ed277cbde5636231b160afb3a7
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-02 21:19:15 +0000

    Modified install instructions
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@1308 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/install.R
+++ www/install.R
@@ -1,8 +1,8 @@
-install.packages(c("geometry", "R.matlab", "ttutils", "rgl", "png", "sp", "RImageJROI"), repos="http://cran.r-project.org")
+install.packages(c("geometry", "R.matlab", "ttutils", "rgl", "png", "sp", "RImageJROI", "RTriangle"), repos="http://cran.r-project.org")
 install.packages(c("retistruct", "retistructdemos"),
                  repos=c("http://www.neuralmapformation.org/R/",
-                         "http://cran.r-project.org",
-                         "http://R-Forge.R-project.org"),
+                     "http://cran.r-project.org",
+                     "http://R-Forge.R-project.org"),
                  type=ifelse(Sys.info()[["sysname"]] == "Windows", "win.binary", "source"))
 
 
