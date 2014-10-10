ea956139ff634455a322cd0b4525fd2602469ce4
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-10 14:32:23 +0000

    Updated install instructions so they just refer to r-forge
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/retistruct@1330 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/install-gui.R
+++ www/install-gui.R
@@ -1,8 +1,3 @@
 install.packages(c("gWidgets", "gWidgetsRGtk2", "cairoDevice"),
                  repos=c("http://cran.r-project.org"))
-install.packages(c("retistructgui"),
-                 repos=c("http://www.neuralmapformation.org/R/",
-                     "http://cran.r-project.org", 
-                     "http://R-Forge.R-project.org"),
-                 type=ifelse(Sys.info()[["sysname"]] == "Windows", "win.binary", "source"))
-library(retistructgui)
+library(retistruct)
--- www/install.R
+++ www/install.R
@@ -1,8 +1,6 @@
-install.packages(c("geometry", "R.matlab", "ttutils", "rgl", "png", "sp", "RImageJROI", "RTriangle"), repos="http://cran.r-project.org")
 install.packages(c("retistruct", "retistructdemos"),
-                 repos=c("http://www.neuralmapformation.org/R/",
-                     "http://cran.r-project.org",
-                     "http://R-Forge.R-project.org"),
+                 repos=c("http://cran.r-project.org",
+                   "http://R-Forge.R-project.org"),
                  type=ifelse(Sys.info()[["sysname"]] == "Windows", "win.binary", "source"))
 
 
