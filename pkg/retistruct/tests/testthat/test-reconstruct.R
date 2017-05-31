be702cc57360e2c32ffc85d2fe70f92236e7e5bc
David C Sterratt <david.c.sterratt@ed.ac.uk>
2017-05-31 18:04:56 +0100

    Made the reconstruct test a testthat one

---
+++ pkg/retistruct/tests/testthat/test-reconstruct.R
@@ -0,0 +1,18 @@
+context("Checking reconstruction")
+test_that("Retina reconstructs correctly", {
+  dataset <- file.path(system.file(package = "retistruct"), "extdata", "GMB530/R-CONTRA")
+  r <- retistruct.read.dataset(dataset)
+  ## Load the human annotation of tears
+  r <- retistruct.read.markup(r)
+  ## Reconstruct
+  r <- retistruct.reconstruct(r)
+
+  ## Save as matlab
+  r$dataset <- tempdir()
+  retistruct.export.matlab(r)
+
+  ## These values obtained from Retistruct 0.5.10
+  expect_equal(r$E.l, 0.001450761, tolerance=0.00001)
+  expect_equal(r$A.tot, 29191727)
+  expect_equal(r$EOD, 4.517927, tolerance=0.01) 
+})
