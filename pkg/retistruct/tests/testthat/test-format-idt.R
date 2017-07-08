8a92d02e19cc6a10f067c0078dc2d75d2a017d6c
David C Sterratt <david.c.sterratt@ed.ac.uk>
2017-07-08 17:05:52 +0100

    Added tests for IDT format

---
+++ pkg/retistruct/tests/testthat/test-format-idt.R
@@ -0,0 +1,18 @@
+context("Checking IDT format")
+test_that("IDT format is read correctly", {
+  dataset <- file.path(system.file(package = "retistruct"), "extdata", "GMB530/R-CONTRA")
+  r <- retistruct.read.dataset(dataset)
+  ## Test that points are read in correctly 
+  P <- as.matrix(read.csv(file.path(dataset, "P.csv")))
+  attr(P, "dimnames") <- NULL
+  expect_that(r$P, equals(P))
+  ## Test that optic disc is read in correctly 
+  ## od <- rbind(c(354, 336),
+  ##               c(354, 347),
+  ##               c(359, 352),
+  ##               c(364, 347),
+  ##               c(364, 338),
+  ##               c(360, 333))
+  ##   colnames(od) <- c("x", "y")
+  ##   expect_that(r$Ss$OD, equals(od))
+})
