951df9fc968423d79a3fa8b082fadaf9c0c28e4f
David C Sterratt <david.c.sterratt@ed.ac.uk>
2017-07-18 12:00:58 +0100

    Removed or moved R-forge legacy files

--- www/install-gui.R
+++
@@ -1,4 +0,0 @@
-install.packages(c("gWidgets", "gWidgetsRGtk2", "cairoDevice"),
-                 repos=c("http://cran.r-project.org"))
-library(retistruct)
-retistruct()
--- www/install.R
+++
@@ -1,6 +0,0 @@
-install.packages(c("retistruct", "retistructdemos"),
-                 repos=c("http://cran.r-project.org",
-                   "http://R-Forge.R-project.org"),
-                 type=ifelse(Sys.info()[["sysname"]] == "Windows", "win.binary", "source"))
-
-
