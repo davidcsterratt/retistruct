10a0dd7baca16a584a798694b81a51f531ee451c
David C Sterratt <david.c.sterratt@ed.ac.uk>
2023-02-04 11:07:46 +0000

    Renamed deprecated rgl.*functions

--- pkg/retistruct/R/ReconstructedOutline.R
+++ pkg/retistruct/R/ReconstructedOutline.R
@@ -1260,7 +1260,7 @@ sphericalplot.ReconstructedOutline <- function(r,
   ## Obtain Cartesian coordinates of points
   Ps <- r$getPoints()
   P <- sphere.spherical.to.sphere.cart(Ps)
-  rgl.clear()
+  clear3d()
   if (surf) {
     ## Outer triangles
     fac <- 1.005
@@ -1286,13 +1286,13 @@ sphericalplot.ReconstructedOutline <- function(r,
 
   ht <- r$ht
   gb <- r$ol$gb
-  rgl.lines(fac*rbind(P[ht[gb[gb]],1], P[ht[gb],1]),
+  lines3d(fac*rbind(P[ht[gb[gb]],1], P[ht[gb],1]),
             fac*rbind(P[ht[gb[gb]],2], P[ht[gb],2]),
             fac*rbind(P[ht[gb[gb]],3], P[ht[gb],3]),
             lwd=3, color=getOption("TF.col"))
   
   fac <- 1.006
-  rgl.lines(fac*rbind(P[ht[gb[gb]],1], P[ht[gb],1]),
+  lines3d(fac*rbind(P[ht[gb[gb]],1], P[ht[gb],1]),
             fac*rbind(P[ht[gb[gb]],2], P[ht[gb],2]),
             fac*rbind(P[ht[gb[gb]],3], P[ht[gb],3]),
             lwd=3, color=getOption("TF.col"))
